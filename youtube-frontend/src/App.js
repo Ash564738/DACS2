019d1e3f1b77c9e146e301d37ed6922e2ccf76f0
Ash564738 <kieutrang5647@gmail.com>
2024-11-03 22:26:04 +0700

    create homepage

--- youtube-frontend/src/App.js
+++ youtube-frontend/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import Navbar from './Component/Navbar/navbar';
+import Home from './Pages/Home/home';
+import { useState } from 'react';
+import {Route, Routes} from 'react-router-dom';
+import Video from './Pages/Video/video';
+import axios from 'axios';
 function App() {
+  const [sideNavbar,setSideNavbar] = useState(true);
+  const setSideNavbarFunc=(value)=>{
+    setSideNavbar(value)
+  }
   return (
     <div className="App">
-      <Navbar />
+      <Navbar setSideNavbarFunc={setSideNavbarFunc} sideNavbar={sideNavbar} />
+      <Routes>
+        <Route path='/' element={<Home sideNavbar={sideNavbar} />} />
+        <Route path='/watch/:id' element={<Video />} />
+      </Routes>
     </div>
   );
 }
-
 export default App;
