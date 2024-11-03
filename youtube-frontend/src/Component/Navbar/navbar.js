9e5bb560103b83a8bbf7ca14cc4da1507ceaa599
Ash564738 <kieutrang5647@gmail.com>
2024-11-03 13:29:56 +0700

    nav bar

--- youtube-frontend/src/Component/Navbar/navbar.js
+++ youtube-frontend/src/Component/Navbar/navbar.js
@@ -6,6 +6,14 @@ import YoutubeIcon from '@mui/icons-material/YouTube';
 import KeyBoardVoiceIcon from '@mui/icons-material/KeyboardVoice';
 import VideoCallIcon from '@mui/icons-material/VideoCall';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
+import Brightness3Icon from '@mui/icons-material/Brightness3';
+import SettingsIcon from '@mui/icons-material/Settings';
+import TranslateIcon from '@mui/icons-material/Translate';
+import LanguageIcon from '@mui/icons-material/Language';
+import FeedbackOutlinedIcon from '@mui/icons-material/FeedbackOutlined';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
 const Navbar = () => {
     const [userPic, setUserPic] = useState("https://i.kym-cdn.com/entries/icons/original/000/043/403/cover3.jpg");
     return (
@@ -37,9 +45,26 @@ const Navbar = () => {
                     <NotificationsIcon sx={{ color: "white", fontSize: "30px", cursor: "pointer" }} />
                     <img src={userPic} className="header__userava" alt="Ava"/>
                     <div className="header-modal">
-                        <div className="header-modal-option">Profile</div>
-                        <div className="header-modal-option">Setting</div>
-                        <div className="header-modal-option">Logout</div>
+                        <div className="header-modal-channel">
+                            <img src={userPic} className="header__userava" alt="Ava"/>
+                            <div className="header-modal-channel-inf">
+                                <div className="header-modal-channel-name">Catto</div>
+                                <div className="header-modal-channel-email">@Catto.gmail.com</div>
+                                <div className="header-modal-channel-profile">View your channel</div>
+                            </div>
+                        </div>
+                        <hr className="header-modal-separator" />
+                        <div className="header-modal-option">
+                            <LoginIcon/>Sign In</div>
+                        <div className="header-modal-option"><LogoutIcon/>Sign Out</div>
+                        <hr className="header-modal-separator" />
+                        <div className="header-modal-option"><Brightness3Icon/>Appearance: Dark</div>
+                        <div className="header-modal-option"><TranslateIcon/>Language: English</div>
+                        <div className="header-modal-option"><LanguageIcon/>Location: US</div>
+                        <hr className="header-modal-separator"/>
+                        <div className="header-modal-option"><SettingsIcon/>Settings</div>
+                        <div className="header-modal-option"><HelpOutlineOutlinedIcon/>Help</div>
+                        <div className="header-modal-option"><FeedbackOutlinedIcon/>Send Feedback</div>
                     </div>
                 </div>
             </div>
