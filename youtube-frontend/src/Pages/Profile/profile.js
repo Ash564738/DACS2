7fa1209ffa68074064dfc9c1fedc3d3bc5a9b9a3
Ash564738 <kieutrang5647@gmail.com>
2024-11-05 13:34:31 +0700

    add some file

--- youtube-frontend/src/Pages/Profile/profile.js
+++ youtube-frontend/src/Pages/Profile/profile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import './profile.css';
 import SideNavbar from '../../Component/SideNavbar/sideNavbar';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
@@ -9,26 +9,33 @@ const Profile = ({ sideNavbar }) => {
     const [data, setData] = useState([]);
     const [user, setUser] = useState(null);
     const fetchProfileData = async () => {
-        {/* Please watch the video for the code} */}
-    }
+        try {
+            const userResponse = await axios.get(`http://localhost:4000/api/getUserById/${id}`);
+            setUser(userResponse.data.user);
+            const videosResponse = await axios.get(`http://localhost:4000/api/getVideosByUserId/${id}`);
+            setData(videosResponse.data.videos);
+        } catch (error) {
+            console.error("Error fetching profile data:", error);
+        }
+    };
     useEffect(() => {
-        fetchProfileData()
-    }, [])
+        fetchProfileData();
+    }, [id]);
     return (
         <div className='profile'>
             <SideNavbar sideNavbar={sideNavbar} />
             <div className={sideNavbar ? "profile_page" : "profile_page_inactive"}>
                 <div className="profile_top_section">
                     <div className="profile_top_section_profile">
-                        <img className='profile_top_section_img' src={user?.profilePic} alt="" />
+                        <img className='profile_top_section_img' src={user?.profilePic} alt="Profile" />
                     </div>
                     <div className="profile_top_section_About">
-                        <div className="profile_top_section_About_Name">ABC</div>
+                        <div className="profile_top_section_About_Name">{user?.channelName}</div>
                         <div className="profile_top_section_info">
-                            @ABC . 4 videos . 100 subscribers
+                            @{user?.username} . {data.length} videos . {user?.subscribersCount || 0} subscribers
                         </div>
                         <div className="profile_top_section_info">
-                            About abc
+                            About {user?.bio || user?.channelName}
                         </div>
                     </div>
                 </div>
@@ -36,25 +43,22 @@ const Profile = ({ sideNavbar }) => {
                     <div className="profile_videos_title">Videos &nbsp; <ArrowRightIcon /></div>
                     <div className="profileVideos">
                         {
-                            data.map((item, key) => {
-                                return (
-                                    <Link to={`/watch/${item._id}`} className="profileVideo_block">
-                                        <div className="profileVideo_block_thumbnail">
-                                            <img src={item.thumbnail} alt="thumbnail" className="profileVideo_block_thumbnail_img" />
-                                        </div>
-                                        <div className="profileVideo_block_detail">
-                                            <div className="profileVideo_block_detail_name">Video Title</div>
-                                            <div className="profileVideo_block_detail_about">Create at 2024-09-12</div>
-                                        </div>
-                                    </Link>
-                                );
-                            })
+                            data.map((item, key) => (
+                                <Link to={`/watch/${item._id}`} className="profileVideo_block" key={item._id}>
+                                    <div className="profileVideo_block_thumbnail">
+                                        <img src={item.thumbnail} alt="thumbnail" className="profileVideo_block_thumbnail_img" />
+                                    </div>
+                                    <div className="profileVideo_block_detail">
+                                        <div className="profileVideo_block_detail_name">{item.title}</div>
+                                        <div className="profileVideo_block_detail_about">Created at {new Date(item.createdAt).toLocaleDateString()}</div>
+                                    </div>
+                                </Link>
+                            ))
                         }
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
-
-export default Profile
+export default Profile;
--- youtube-frontend/src/Pages/SignUp/signUp.js
+++ youtube-frontend/src/Pages/SignUp/signUp.js
@@ -6,7 +6,6 @@ import axios from 'axios';
 import { toast, ToastContainer } from 'react-toastify'
 import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
-
 const SignUp = () => {
     const [uploadedImageUrl, setUploadedImageUrl] = useState("https://th.bing.com/th/id/OIP.Wy2uo_y-ttULYs4chLmqSAAAAA?rs=1&pid=ImgDetMain");
     const [singUpFiled, setSignUpField] = useState({ "channelName": "", "userName": "", "password": "", "about": "", "profilePic": uploadedImageUrl });
@@ -18,7 +17,6 @@ const SignUp = () => {
         })
     }
     console.log(singUpFiled)
-
     const uploadImage = async (e) => {
         console.log("Uploading")
         const files = e.target.files;
@@ -39,15 +37,10 @@ const SignUp = () => {
         } catch (err) {
             console.log(err)
         }
-
-
     }
     const handleSignup = async () => {
         {/* Please watch the video for the code} */}
     }
-
-
-
     return (
         <div className='signUp'>
             <div className="signup_card">
@@ -55,36 +48,25 @@ const SignUp = () => {
                     <YouTubeIcon sx={{ fontSize: "54px" }} className='login_youtubeImage' />
                     SignUp
                 </div>
-
                 <div className="signUp_Inputs">
-                    
                     {/* Please watch the video for the code} */}
-
-
                     <div className="image_upload_signup">
                         <input type='file' onChange={(e) => uploadImage(e)} />
                         <div className='image_upload_signup_div'>
                             <img className='image_default_signUp' src={uploadedImageUrl} />
                         </div>
                     </div>
-
-
                     <div className="signUpBtns">
                         <div className="signUpBtn" onClick={handleSignup}>SignUp</div>
                         <Link to={'/'} className="signUpBtn">Home Page</Link>
-
                     </div>
-
                     {progressBar && <Box sx={{ width: '100%' }}>
                         <LinearProgress />
                     </Box>}
-
                 </div>
-
             </div>
             <ToastContainer />
         </div>
     )
 }
-
 export default SignUp
