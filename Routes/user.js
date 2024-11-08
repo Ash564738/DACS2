5bb5013b41b8b9fa05c387de365859489700a71e
Ash564738 <kieutrang5647@gmail.com>
2024-11-08 12:49:30 +0700

    handle user data

--- Routes/user.js
+++ Routes/user.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../Controllers/user');
 router.post("/signUp",UserController.signUp)
-router.post('/login',UserController.signIn);
-router.post('/logout',UserController.logout);
+router.post('/signIn',UserController.signIn);
+router.post('/logOut',UserController.logOut);
 module.exports = router;
