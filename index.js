cf457fc9720e1ba0d2ce64e906e8e1f20f39874a
Ash564738 <kieutrang5647@gmail.com>
2024-11-08 12:49:30 +0700

    handle user data

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var express = require("express");
 var app = express();
 var port = 4000
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
+app.use(express.json());
+app.use(cookieParser());
+require('./Connection/conn');
+const AuthRoutes = require('./Routes/user');
+const VideoRoutes = require('./Routes/video');
+const CommentRoutes = require('./Routes/comment');
+app.use('/auth',AuthRoutes);
+// app.use('/api',VideoRoutes);
+// app.use('/commentApi',CommentRoutes);
+// app.listen(port,()=>{console.log("Our backend project is running on Port 4000")});
 app.get("/", (req, res) => {
     res.send("Hello World");
     }
@@ -9,19 +25,3 @@ app.listen(port, () => {
     console.log("Our backend project is running on Port 4000")
 }
 );
-// const cookieParser = require('cookie-parser');
-// const cors = require('cors');
-// app.use(cors({
-//     origin: 'http://localhost:3000',
-//     credentials: true
-// }))
-// app.use(express.json());
-// app.use(cookieParser());
-// require('./Connection/conn');
-// const AuthRoutes = require('./Routes/user');
-// const VideoRoutes = require('./Routes/video');
-// const CommentRoutes = require('./Routes/comment');
-// app.use('/auth',AuthRoutes);
-// app.use('/api',VideoRoutes);
-// app.use('/commentApi',CommentRoutes);
-// app.listen(port,()=>{console.log("Our backend project is running on Port 4000")});
