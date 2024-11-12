2d209b2b8e8c73e64492df51fb794305bfe59994
Ash564738 <kieutrang5647@gmail.com>
2024-11-12 12:54:36 +0700

    abc

--- Connection/conn.js
+++ Connection/conn.js
@@ -4,3 +4,12 @@ mongoose
   .then(() => console.log('DB connection successful!')).catch(err=>{
     console.log(err)
   });
+//VM
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://127.0.0.1:27017/DACS2')
+.then(() => console.log('DB connection successful!'))
+.catch(err => console.log(err));
+
+  // mongoose.connect('mongodb://192.168.101.23:27017/DACS2')
+  // .then(() => console.log('DB connection successful!'))
+  // .catch(err => console.log(err));
