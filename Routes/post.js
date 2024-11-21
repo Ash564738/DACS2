f08bbfbb8eee593e43c1ca2f510cd9091df6064c
Ash564738 <kieutrang5647@gmail.com>
2024-11-21 11:02:13 +0700

    synch

--- Routes/post.js
+++ Routes/post.js
@@ -2,23 +2,13 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../Controllers/post');
 const auth = require('../middleware/authentication');
-<<<<<<< HEAD
-
-=======
->>>>>>> d220ad6971b31171f3e30efb4fcfbb14c1355e4e
 // Route to create a new post
 router.post('/createPost', postController.createPost);
 // Route to get all posts
 router.get('/getAllPosts', postController.getAllPosts);;
 // Route to add a comment to a post
 router.post('/:id/comments', auth, postController.addCommentToPost);
-<<<<<<< HEAD
 // Route to like a post
 router.post('/:id/like', auth, postController.likePost);
-=======
-router.put('/:id', postController.toggleLikeDislike);
-router.put('/:postId/comments/:commentId/like', postController.likeComment);
-router.put('/:postId/comments/:commentId/dislike', postController.dislikeComment);
->>>>>>> d220ad6971b31171f3e30efb4fcfbb14c1355e4e
 
 module.exports = router;
--- youtube-frontend/src/Component/HomePage/homePage.jsx
+++ youtube-frontend/src/Component/HomePage/homePage.jsx
@@ -30,10 +30,6 @@ const HomePage = ({ sideNavbar }) => {
   }, []);
   useEffect(() => {
     if (data.length === 0) return;
-<<<<<<< HEAD
-
-=======
->>>>>>> d220ad6971b31171f3e30efb4fcfbb14c1355e4e
     const slider = document.querySelector('.homePage_options');
     if (!slider) {
         console.warn("Slider not found.");
@@ -73,12 +69,7 @@ const HomePage = ({ sideNavbar }) => {
         slider.removeEventListener('mouseup', handleMouseUp);
         slider.removeEventListener('mousemove', handleMouseMove);
     };
-<<<<<<< HEAD
-}, [data]);
-=======
   }, [data]);
-
->>>>>>> d220ad6971b31171f3e30efb4fcfbb14c1355e4e
   const options = [
     "All",
     "Music",
